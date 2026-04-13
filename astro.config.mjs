b1977c4529c9fddc3859fbaa8958ad49525fc942
gabriel <nextzyagency@gmail.com>
2026-04-13 11:06:48 -0500

    fix(config): remove base path for correct css loading in cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nextzyagency.github.io',
-  base: '/go-essence',
   integrations: [tailwind()],
 });
